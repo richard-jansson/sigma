6fd1c9d160d8a8f377266d769606c402b18ed61f
Richard J. Jansson <richard.j.jansson@gmail.com>
2018-12-05 13:24:19 +0200

    Scripting to create levels

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cp pg768.txt tmp.txt
+
+CHAPTER=0
+while true;do
+	echo "wc -l " 
+	wc -l tmp.txt
+	CHPLFT=$( grep -n CHAPTER tmp.txt | wc -l | cut -d " " -f1 ) 
+	echo "CHPLFT: $CHPLFT"
+	if [[ $CHPLFT -eq "0" ]];then 
+		break;
+	fi
+	START=$( grep -n CHAPTER tmp.txt | cut -d ":" -f1 | head -n1)
+	END=$( grep -n CHAPTER tmp.txt | cut -d ":" -f1 | head -n2 | tail -n1)
+	
+	ENDA=$( wc -l tmp.txt | cut -d " " -f1) 
+	
+	LEN=$(( END-START -3 ))
+	echo "S=$START E=$END L=$LEN"
+	
+	head -n $(( END -1 )) tmp.txt | tail -n $LEN > chapters/$CHAPTER.txt
+	echo "CHAPTER: $CHAPTER"
+	CHAPTER=$(( CHAPTER+1 ))
+
+	cp tmp.txt foo.txt
+	tail -n $(( ENDA - $END )) foo.txt > tmp.txt
+done
+
+#CHAPTER=$(( CHAPTER+1 ))
+echo "CHAP: $CHAPTER"
+ENDA=$( wc -l tmp.txt | cut -d " " -f1) 
+
+
+tail -n $(( ENDA -2 ))  tmp.txt > chapters/$CHAPTER.txt
