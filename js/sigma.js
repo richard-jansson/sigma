324466117669aec79b18a878b2cc3016726ca562
Richard J. Jansson <richard.j.jansson@gmail.com>
2018-12-07 00:09:12 +0200

    basic game logic

--- js/sigma.js
+++ js/sigma.js
@@ -1,9 +1,11 @@
 var curr;
+var pword="";
+var cword="";
 
 function __drawString(s){
 	var tdim=ctx.measureText(s);
 
-	this.ctx.strokeStyle="white";
+	this.ctx.strokeStyle=this.style;
 	this.ctx.font="24px PressStart2P";
 
 	if( tdim.width + this.x > 640 ){
@@ -11,18 +13,22 @@ function __drawString(s){
 		this.y+=36;
 	}
 	if( 36 + this.y> this.h+this.y0){
-//		console.log("x0:"+this.x0+", y0:"+this.y0);
-		this.ctx.fillRect(this.x0,this.y0,this.w,this.h);
-		this.x=this.x0;
-		this.y=this.y0;
+		this.clear();
 	}
 	this.ctx.strokeText(s,this.x,this.y+36);
 	var tdim=ctx.measureText(s);
 	this.x+=tdim.width;
 }
 
-function textArea(ctx,x,y,w,h){
-	return {ctx:ctx,x0:x,y0:y,w:w,h:h,print:__drawString,x:x,y:y};
+function __clearString(){
+	this.ctx.fillRect(this.x0,this.y0,this.w,this.h);
+	this.x=this.x0;
+	this.y=this.y0;
+}
+
+function textArea(ctx,x,y,w,h,style){
+	return {ctx:ctx,x0:x,y0:y,w:w,h:h,print:__drawString,x:x,y:y,style:style,
+		clear:__clearString};
 }
 
 
@@ -33,6 +39,7 @@ function doWord(){
 	curr=curr.substr(i+1);
 	
 	gametext.print(w);
+	cword=w;
 }
 
 function init(){
@@ -43,17 +50,48 @@ function init(){
 
 	curr=levels[0][0];		
 
-	gametext=new textArea(ctx,36,36,640,480-36*4);
+	gametext=new textArea(ctx,36,36,640,480-36*4,"white");
+	playertext=new textArea(ctx,36,480-36*2,640,480-36*4,"red");
 
-//	doWord();
-	setInterval(doWord,100);
+	doWord();
+//	setInterval(doWord,100);
 }
 
 function doKey(key){
 	console.log(key);		
+
+	playertext.print(key);
+
+	pword+=key;
+	
+	console.log(pword+" = "+cword);
+
+	var pmatch=cword.substr(0,pword.length);	
+	if(pmatch == pword){
+		console.log("partial match...");
+	}
+
+	if(pword.toLowerCase() == cword.substr(0,cword.length-1).toLowerCase() || cword==" "){
+		console.log("Match!");
+		doWord();
+		pword="";
+		playertext.clear();
+	}
 }
 
-document.onkeyup=function(e){ tmp=e; if(e.key.length<3) doKey(e.key)};
+function doDelete(){
+	if(pword.length<1) return;
+	pword=pword.substr(0,pword.length-1);
+
+	playertext.clear();
+	playertext.print(pword);
+}
+
+document.onkeyup=function(e){ 
+	tmp=e; 
+	if(e.key=="Backspace") doDelete();
+	if(e.key.length<3) doKey(e.key)
+};
 
 if(document.readyState=="complete" || (document.readyState!="loading" && document.documentElement.doScroll)) init();
 else document.addEventListener("DOMContentLoaded",init);
