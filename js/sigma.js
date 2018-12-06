a2d2dd1a4f03439a9d02718371cee804a78e0ea3
Richard J. Jansson <richard.j.jansson@gmail.com>
2018-12-07 00:45:24 +0200

    fixes touchups

--- js/sigma.js
+++ js/sigma.js
@@ -1,6 +1,12 @@
 var curr;
 var pword="";
 var cword="";
+var paragraph=0;
+var level=0;
+var n_levels=0;
+
+var words=0;
+var t0;
 
 function __drawString(s){
 	var tdim=ctx.measureText(s);
@@ -31,10 +37,17 @@ function textArea(ctx,x,y,w,h,style){
 		clear:__clearString};
 }
 
-
 function doWord(){
 	var i=curr.indexOf(" ");
 	var w=curr.substr(0,i+1);
+
+	if(curr.length==0) curr=levels[level][++paragraph];
+	if(curr.length==0) curr=levels[++level][0];
+	if(curr.length==0) {
+		// FIXME 
+		console.log("GaMe OvEr!");
+	}
+
 
 	curr=curr.substr(i+1);
 	
@@ -43,6 +56,8 @@ function doWord(){
 }
 
 function init(){
+	for(var k in levels) n_levels++;
+
 	console.log("init");
 	var canvas=document.getElementById("canvas");
 
@@ -50,11 +65,24 @@ function init(){
 
 	curr=levels[0][0];		
 
+	t0=new Date();
+
+	wpm=new textArea(ctx,0,0,640,36,"green");
 	gametext=new textArea(ctx,36,36,640,480-36*4,"white");
 	playertext=new textArea(ctx,36,480-36*2,640,480-36*4,"red");
 
 	doWord();
 //	setInterval(doWord,100);
+}
+
+function doWPM(){
+	words++;
+	var t=new Date();
+	var dt=(t-t0)/1000;
+	var wps=words/dt;
+
+	wpm.clear();
+	wpm.print( Math.floor(wps*60) +" WPM Lvl: "+(level+1)+"/"+n_levels +" "+Math.ceil(100*paragraph/levels[level].length)+"%");
 }
 
 function doKey(key){
@@ -64,19 +92,22 @@ function doKey(key){
 
 	pword+=key;
 	
-	console.log(pword+" = "+cword);
+	console.log("{"+pword+"} = {"+cword+"}");
 
 	var pmatch=cword.substr(0,pword.length);	
 	if(pmatch == pword){
 		console.log("partial match...");
 	}
 
-	if(pword.toLowerCase() == cword.substr(0,cword.length-1).toLowerCase() || cword==" "){
+	if(pword.toLowerCase() == cword.trimEnd().toLowerCase()){
 		console.log("Match!");
 		doWord();
 		pword="";
 		playertext.clear();
+		
+		doWPM();
 	}
+	while(cword.trim().length == 0) doWord();
 }
 
 function doDelete(){
