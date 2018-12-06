a3fb38b822fbe99d81430b25de8c03f2c79216f0
Richard J. Jansson <richard.j.jansson@gmail.com>
2018-12-06 23:51:24 +0200

    object for textarea

--- js/sigma.js
+++ js/sigma.js
@@ -1,47 +1,59 @@
 var curr;
-var x=0;
-var y=0;
+
+function __drawString(s){
+	var tdim=ctx.measureText(s);
+
+	this.ctx.strokeStyle="white";
+	this.ctx.font="24px PressStart2P";
+
+	if( tdim.width + this.x > 640 ){
+		this.x=this.x0;
+		this.y+=36;
+	}
+	if( 36 + this.y> this.h+this.y0){
+//		console.log("x0:"+this.x0+", y0:"+this.y0);
+		this.ctx.fillRect(this.x0,this.y0,this.w,this.h);
+		this.x=this.x0;
+		this.y=this.y0;
+	}
+	this.ctx.strokeText(s,this.x,this.y+36);
+	var tdim=ctx.measureText(s);
+	this.x+=tdim.width;
+}
+
+function textArea(ctx,x,y,w,h){
+	return {ctx:ctx,x0:x,y0:y,w:w,h:h,print:__drawString,x:x,y:y};
+}
+
 
 function doWord(){
 	var i=curr.indexOf(" ");
 	var w=curr.substr(0,i+1);
 
 	curr=curr.substr(i+1);
-
-
-	var tdim=ctx.measureText(w);
-	console.log(x+","+y);
-	console.log(tdim.width + " " + tdim.height);
-	if( tdim.width + x > 640 ){
-		x=36;
-		y+=36;
-	}
-	if( 36 + y> 480){
-		ctx.fillRect(0,0,640,480);
-		x=36;
-		y=36;
-	}
-	ctx.strokeText(w,x,y);
-	x+=tdim.width;
+	
+	gametext.print(w);
 }
 
 function init(){
 	console.log("init");
 	var canvas=document.getElementById("canvas");
 
-	x=36;
-	y=36;
-
 	ctx=canvas.getContext("2d");
-
-	ctx.strokeStyle="white";
-
-	ctx.font="24px PressStart2P";
 
 	curr=levels[0][0];		
 
-	setInterval(doWord,500);
+	gametext=new textArea(ctx,36,36,640,480-36*4);
+
+//	doWord();
+	setInterval(doWord,100);
 }
+
+function doKey(key){
+	console.log(key);		
+}
+
+document.onkeyup=function(e){ tmp=e; if(e.key.length<3) doKey(e.key)};
 
 if(document.readyState=="complete" || (document.readyState!="loading" && document.documentElement.doScroll)) init();
 else document.addEventListener("DOMContentLoaded",init);
