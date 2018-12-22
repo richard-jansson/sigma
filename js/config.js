225a230f80d0894329d245b5e3577f4461129379
Richard J. Jansson <richard.j.jansson@gmail.com>
2018-12-22 10:28:02 +0200

    rendering options for chinese tree

--- js/config.js
+++ js/config.js
@@ -17,8 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 // Time in ms for animation 
-var ANIMT=10;
-var FPS=20;
+var ANIMT=5;
+var FPS=5;
 // keys to select leaves 
 var dkeys=["W","D","S","A"];
 // keys to select branches
