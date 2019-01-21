1ba79e11c88c86eab9e797df8eb3235acf63c9d9
Richard J. Jansson <richard.j.jansson@gmail.com>
2019-01-21 22:30:30 +0200

    login

--- js/login.js
+++ js/login.js
@@ -1,12 +1,35 @@
 function login(){
+    var uname=$("#luname").val();
+    var rpassword=$("#lpassword").val();
+
+    if(typeof(uname)=="undefined" || uname.length==0){ 
+        $("#login_msg").html("Enter an username");
+        $("#login_msg").show();
+        return
+    }
+    if(typeof(rpassword)=="undefined" || rpassword.length==0){ 
+        $("#login_msg").html("Enter a password");
+        $("#login_msg").show();
+        return
+    }
+    jQuery.get("login.php",{uname: uname, pass: rpassword},function(d){
+        var ro=JSON.parse(d);
+        if(ro.status=="failed"){
+            $("#login_msg").html(ro.msg);
+            $("#login_msg").show();
+        }else{
+            $("#login_msg").html("");
+            console.log("success");
+        }
+    }).fail(function(){
+        $("#login_msg").html("Failed to submit request, please try again.");
+        $("#login_msg").show();
+    })
 }
 function register(){
     var uname=$("#runame").val();
     var rpassword=$("#rpassword").val();
     var mail=$("#rmail").val();
-
-    console.log("register");
-    console.log(uname)
     
     if(typeof(uname)=="undefined" || uname.length==0){ 
         $("#register_msg").html("Enter an username");
@@ -26,7 +49,7 @@ function register(){
             $("#register_msg").html(ro.msg);
             $("#register_msg").show();
         }else{
-            window.location="conf.html";
+//            window.location="conf.html";
         }
     }
     ).fail(function(){
--- login.php
+++ login.php
@@ -12,8 +12,7 @@ $uname=mysqli_real_escape_string($con,$_GET["uname"]);
 $pass=mysqli_real_escape_string($con,$_GET["pass"]);
 $mail=mysqli_real_escape_string($con,$_GET["mail"]);
 
-
-$query = "SELECT COUNT(*) as c from users where username = \"$uname\"";
+$query = "SELECT COUNT(*) as c from users where username = \"$uname\" and password = PASSWORD(\"$pass\");";
 $r=mysqli_query($con,$query);
 if(!$r){
     $msg=array("status"=>"failed","msg"=>"database error, query failed",
@@ -22,26 +21,12 @@ if(!$r){
     die();
 }
 $row=mysqli_fetch_assoc($r);
-//print_r($row);
-if($row["c"] > 0) {
-    $msg=array("status"=>"failed","msg"=>"username taken");
+if($row["c"] < 1) {
+    $msg=array("status"=>"failed","msg"=>"authentication failed");
     print json_encode($msg);
     die();
 }
 
-$query = "INSERT INTO users (username,password,email) " .
-        " VALUES " . 
-         "(\"".$uname."\",PASSWORD(\"".$pass."\"),\"".$mail."\");";
-
-$r=mysqli_query($con,$query);
-if(!$r){
-    $msg=array("status"=>"failed","msg"=>"database error, query failed",
-    "error"=> mysqli_error($con));
-    print json_encode($msg);
-    die();
-}
 $msg=array("status"=>"success");
-
 echo json_encode($msg);
-
 ?>
