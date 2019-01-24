a9fdcdf008386eb20931911348ac60f98aa9b0b3
Richard J. Jansson <richard.j.jansson@gmail.com>
2019-01-24 12:29:32 +0200

    add new key bindings

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,14 @@
+var expecting_inp=false;
+var curr_signal=false;
+
 window.onkeyup=function(e){
     console.log("stub");
 }
 window.onkeydown=function(e){
-    console.log("stub");
+    if(expecting_inp){
+        console.log(e.code);
+        addBindAct(curr_signal,e.code);
+    }
 }
 
 function saveBind(){
@@ -10,10 +16,10 @@ function saveBind(){
 }
 
 function delBind(){
-    var signal=$(this).parent(".group").attr("signal"); 
-    var weapon=$(this).parent(".settings").attr("weapon"); 
+    var signal=$(this).parents(".group").attr("signal"); 
+    var weapon=$(this).parents(".settings").attr("weapon"); 
 
-    var curr=$(this).parent().parent();
+    var curr=$(this).parent();
 
     console.log("del "+signal+" " + weapon);
 
@@ -22,10 +28,47 @@ function delBind(){
     saveBind();
 }
 
+function dontExpKey(){
+    expecting_inp=false;
+    $(".msg").hide();
+}
+function expKey(){
+    expecting_inp=true;
+    $(".msg span").html("Enter key / btn / axis for "+curr_signal);
+    $(".msg").show();
+}
+
+function addBindAct(signal,code){
+    dontExpKey();
+
+    console.log(signal,code);
+
+    var group=$(".group[signal='"+signal+"']");
+    var key=$("<div></div>",{class:"key"});
+    var tc=$("<span>"+code+"</span>");
+    var td=$("<span>(X)</span>",{class:"del"});
+    td.click(delBind);
+    key.append(tc);
+    key.append(td);
+    
+    var keys=group.find(".keys");
+    keys.append(key);
+}
+
+function addBind(){
+    var signal=$(this).parents(".group").attr("signal"); 
+
+
+    curr_signal=signal;
+    expKey();
+}
+
 function setupKeys(){
     $(".keys .del").click(delBind);
+    $(".group .newbind").click(addBind);
 }
 
 $(document).ready(function(){
+    initgamepad();
     setupKeys();
 });
---
+++ settings2.php
@@ -0,0 +1,46 @@
+<?php
+header('Content-type: text/javascript');
+session_start();
+if(!isset($_SESSION["user_id"])){
+    die("var is_loggedin=false");
+}
+include "cfg.php";
+include "common.php";
+
+$con=mysqli_connect($db_host,$db_user,$db_pass,$db_db);
+if(!$con){
+    $msg=array("status"=>"failed","msg"=>"database error, failed to connect");
+    print json_encode($msg);
+    die();
+}
+
+$q="SELECT * FROM config WHERE user_id = ".$_SESSION["user_id"].";";
+
+$r=mysqli_query($con,$q);
+if(!$r) die();
+$cfgs=array();
+while($row=mysqli_fetch_assoc($r)){
+    if(!in_array($row["cfg_key"],$allowed)) continue;
+
+    $cfgs[$row["cfg_key"]]=$row["cfg_val"];
+}
+
+
+?>
+var is_loggedin = true;
+var user_name = "<?php echo $_SESSION["username"]; ?>";
+var user_id = <?php echo $_SESSION["user_id"]; ?>;
+<?php
+foreach($cfgs as $k => $v){ 
+?>
+var <?php echo $k ?> = "<?php echo $v; ?>";
+<?php
+}
+?>
+
+var GAMEPAD_POLL_INT=10
+var GAMEPAD_AXIS_TRESH0=0.4;
+var GAMEPAD_AXIS_TRESH1=0.4;
+
+var GAMEPAD_MIN_T=80
+var BTN_MIN_T=80
